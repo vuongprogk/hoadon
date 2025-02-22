0281792153b0e1a98efda585f843c5ef1bbe7f7c
vuongprogk <vuong753333@gmail.com>
2025-02-22 13:51:53 +0700

    feat: add C models

---
+++ hoadon/MyModels/CChitiethoadon.cs
@@ -0,0 +1,72 @@
+﻿using hoadon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hoadon.MyModels
+{
+    class CChitiethoadon
+    {
+        public string Sohd { get; set; }
+        public string Mahang { get; set; }
+        public double? Dongia { get; set; }
+        public int? Soluong { get; set; }
+        public string Tenhang
+        {
+            get
+            {
+                if (MahangNavigation == null)
+                {
+                    return string.Empty;
+                }
+                return MahangNavigation.Tenhang;
+            }
+        }
+        public double? Thanhtien
+        {
+            get
+            {
+                if (!Dongia.HasValue || !Soluong.HasValue)
+                {
+                    return 0;
+                }
+                return Dongia.Value * Soluong.Value;
+            }
+        }
+        public string Dvt
+        {
+            get
+            {
+                if (MahangNavigation == null)
+                {
+                    return string.Empty;
+                }
+                return MahangNavigation.Dvt;
+            }
+        }
+        public Hanghoa MahangNavigation { get; set; }
+        public static CChitiethoadon FromChitiethoadon(Chitiethoadon c)
+        {
+            return new CChitiethoadon
+            {
+                Sohd = c.Sohd,
+                Mahang = c.Mahang,
+                Dongia = c.Dongia,
+                Soluong = c.Soluong,
+                MahangNavigation = c.MahangNavigation
+            };
+        }
+        public static Chitiethoadon ToChitiethoadon(CChitiethoadon c)
+        {
+            return new Chitiethoadon
+            {
+                Sohd = c.Sohd,
+                Mahang = c.Mahang,
+                Dongia = c.Dongia,
+                Soluong = c.Soluong,
+            };
+        }
+    }
+}
---
+++ hoadon/MyModels/CHoadon.cs
@@ -0,0 +1,50 @@
+﻿using hoadon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hoadon.MyModels
+{
+    class CHoadon
+    {
+        public CHoadon()
+        {
+            Chitiethoadons = new HashSet<CChitiethoadon>();
+        }
+
+        public string Sohd { get; set; }
+        public DateTime? Ngaylaphd { get; set; }
+        public string Tenkh { get; set; }
+        public double? Thanhtien
+        {
+            get
+            {
+                return Chitiethoadons.Sum(c => c.Thanhtien);
+            }
+        }
+
+        public ICollection<CChitiethoadon> Chitiethoadons { get; set; }
+        public static CHoadon FromHoadon(Hoadon h)
+        {
+            return new CHoadon
+            {
+                Sohd = h.Sohd,
+                Ngaylaphd = h.Ngaylaphd,
+                Tenkh = h.Tenkh,
+                Chitiethoadons = h.Chitiethoadons.Select(c => CChitiethoadon.FromChitiethoadon(c)).ToList()
+            };
+        }
+        public static Hoadon ToHoadon(CHoadon c)
+        {
+            return new Hoadon
+            {
+                Sohd = c.Sohd,
+                Ngaylaphd = c.Ngaylaphd,
+                Tenkh = c.Tenkh,
+                Chitiethoadons = c.Chitiethoadons.Select(c => CChitiethoadon.ToChitiethoadon(c)).ToList()
+            };
+        }
+    }
+}
