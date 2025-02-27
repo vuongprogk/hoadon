161c15f969b2a576020fb295af14b4b099c27bc8
vuongprogk <vuong753333@gmail.com>
2025-02-27 14:35:42 +0700

    feat: add handler data change

--- hoadon/MyModels/WindowHangHoaVM.cs
+++ hoadon/MyModels/WindowHangHoaVM.cs
@@ -9,9 +9,18 @@ using System.Windows.Input;
 
 namespace hoadon.MyModels
 {
-    class WindowHangHoaVM
+    class WindowHangHoaVM : CBaseMVVM
     {
-        public List<Hanghoa> Hanghoas { get; set; }
+        public List<Hanghoa> m_listHangHoa { get; set; }
+        public List<Hanghoa> Hanghoas
+        {
+            get { return m_listHangHoa; }
+            set
+            {
+                m_listHangHoa = value;
+                NotifyPropertyChanged("Hanghoas");
+            }
+        }
         public Hanghoa SelectionHangHoa { get; set; } = default!;
         public RelayCommand DeleteCommand { get; set; }
         public WindowHangHoaVM()
@@ -23,12 +32,26 @@ namespace hoadon.MyModels
         }
         public void DeleteExecute(object parameter)
         {
+            var db = new hoadonContext();
+            db.Hanghoas.Remove(SelectionHangHoa);
+            db.SaveChanges();
+            Hanghoas = db.Hanghoas.ToList();
 
         }
-        public  bool DeleteCanExecute(object parameter)
+        public bool DeleteCanExecute(object parameter)
         {
+            if (SelectionHangHoa == null)
+            {
+                return false;
+            }
+            var db = new hoadonContext();
+            var cthd = db.Chitiethoadons.Where(p => p.Mahang == SelectionHangHoa.Mahang).FirstOrDefault();
+            if (cthd != null)
+            {
+                return false;
+            }
             return true;
         }
-        
+
     }
 }
