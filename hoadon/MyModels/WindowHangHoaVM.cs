8c839b13bc436939d5d3a3060310140d48c3bb7a
vuongprogk <vuong753333@gmail.com>
2025-02-27 13:32:33 +0700

    fix: create delete command and bind to view

--- hoadon/MyModels/WindowHangHoaVM.cs
+++ hoadon/MyModels/WindowHangHoaVM.cs
@@ -1,21 +1,34 @@
 ﻿using hoadon.Models;
 using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace hoadon.MyModels
 {
     class WindowHangHoaVM
     {
         public List<Hanghoa> Hanghoas { get; set; }
-        public Hanghoa SelectionHangHoa { get; set; }
+        public Hanghoa SelectionHangHoa { get; set; } = default!;
+        public RelayCommand DeleteCommand { get; set; }
         public WindowHangHoaVM()
         {
             hoadonContext db = new hoadonContext();
             Hanghoas = db.Hanghoas.ToList();
+            DeleteCommand = new RelayCommand(DeleteExecute, DeleteCanExecute);
 
         }
+        public void DeleteExecute(object parameter)
+        {
+
+        }
+        public  bool DeleteCanExecute(object parameter)
+        {
+            return true;
+        }
+        
     }
 }
